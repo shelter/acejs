962fdc9b84c553592e431a4c23dac8c09034a97f
mason <wuchunlei@live.com>
2014-11-19 14:04:16 +0800

    component update

--- gruntfile.js
+++ gruntfile.js
@@ -2,4 +2,27 @@
  * grunt configuration
  */
 
-
+module.exports = function (grunt) {
+    "use strict";
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            src: ['src/*.js']
+        },
+        uglify: {
+            default: {
+                files: [{
+                    expand: true,
+                    cwd: 'src',
+                    src: '**/*.js',
+                    dest: 'dist'
+                }]
+            }
+        }
+    });
+    //load tasks
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    //register task
+    grunt.registerTask('default', ['jshint', 'uglify']);
+};
