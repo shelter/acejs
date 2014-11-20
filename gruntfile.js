f16619918007d065a5a9d9888c25773eb91f2d64
mason <wuchunlei@live.com>
2014-11-20 16:43:13 +0800

    component update

--- gruntfile.js
+++ gruntfile.js
@@ -23,11 +23,10 @@ module.exports = function (grunt) {
             default: {
                 options: {
                     reporter: 'spec',
-                    captureFile: 'results.txt', // Optionally capture the reporter output to a file
                     quiet: false, // Optionally suppress output to standard out (defaults to false)
                     clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
                 },
-                src: ['test/**/*.js']
+                src: ['tests/**/*.js']
             }
         }
     });
