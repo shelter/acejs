3d99a287eb8c8963f67282590a7bbeb3bc9ec41f
mason <wuchunlei@live.com>
2014-11-20 16:28:35 +0800

    component update

--- gruntfile.js
+++ gruntfile.js
@@ -18,11 +18,23 @@ module.exports = function (grunt) {
                     dest: 'dist'
                 }]
             }
+        },
+        mochaTest: {
+            default: {
+                options: {
+                    reporter: 'spec',
+                    captureFile: 'results.txt', // Optionally capture the reporter output to a file
+                    quiet: false, // Optionally suppress output to standard out (defaults to false)
+                    clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
+                },
+                src: ['test/**/*.js']
+            }
         }
     });
     //load tasks
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-mocha-test');
     //register task
     grunt.registerTask('default', ['jshint', 'uglify']);
 };
